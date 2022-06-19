56551691bb6eebb70e54f8fe86ff323b0e4d6ecf
Sounak Saha <sounaksaha999@gmail.com>
2022-06-19 22:09:20 +0530

    dice challenge updated

---
+++ Day 18/Drum Kit Starting Files/index.js
@@ -0,0 +1,12 @@
+document.querySelector("button").addEventListener("click", function ()
+{
+    alert("I got clicked");   
+
+    // What to do when click detected
+})
+
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//     alert("I got clicked");
+// }
