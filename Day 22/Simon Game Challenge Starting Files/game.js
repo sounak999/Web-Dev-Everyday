b69898b8963388aa1acaaf249d4e53754ba00531
Sounak Saha <sounaksaha999@gmail.com>
2022-06-26 03:23:49 +0530

    step 2

---
+++ Day 22/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,9 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
+
