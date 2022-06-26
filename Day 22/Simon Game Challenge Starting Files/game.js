962797537ca987ea9e9ef1a283126d2bc52fdcdb
Sounak Saha <sounaksaha999@gmail.com>
2022-06-26 15:44:19 +0530

    step 7

--- Day 22/Simon Game Challenge Starting Files/game.js
+++ Day 22/Simon Game Challenge Starting Files/game.js
@@ -2,14 +2,30 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+$(document).keydown(function() {
+    if(!started) {
+
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+})
+
 $(".btn").click(function () {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     
     playSound(userChosenColor);
+    animatePress(userChosenColor);
 });
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -22,4 +38,12 @@ function nextSequence() {
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);    
 }
