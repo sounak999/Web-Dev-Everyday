a8cb60ce2f1de87a52ae2e6ad8314bf339b49b4a
Sounak Saha <sounaksaha999@gmail.com>
2022-06-27 02:06:40 +0530

    step 8

--- Day 22/Simon Game Challenge Starting Files/game.js
+++ Day 22/Simon Game Challenge Starting Files/game.js
@@ -1,28 +1,62 @@
+// our available colors
 var buttonColors = ["red", "blue", "green", "yellow"];
+
+
+// computer generated pattern
 var gamePattern = [];
+// user given pattern
 var userClickedPattern = [];
 
+
+// bool variable to check if the game has already started or not
 var started = false;
 var level = 0;
 
-$(document).keydown(function() {
-    if(!started) {
 
+// press any key to start the game
+$(document).keydown(function() {
+    // this block mark the start of the game
+    if(!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
         started = true;
     }
 })
 
+
+// user pattern gets filled up as we press our buttons
 $(".btn").click(function () {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    
+    checkAnswer(userClickedPattern.length-1);
 });
 
+
+// answer checking function
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000)
+        }
+        
+    }
+    else {
+        console.log("wrong");
+    }
+}
+
 function nextSequence() {
+
+    userClickedPattern = [];
+
     level++;
     $("#level-title").text("Level " + level);
 
